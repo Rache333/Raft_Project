4dcb5043e41382da44b3ed877a6bf64b2abf7c15
idangrave <35558166+idangrave@users.noreply.github.com>
2018-02-27 14:43:25 +0200

    Update raft.c

--- raft/raft.c
+++ raft/raft.c
@@ -1,13 +1,18 @@
 #include "raft.h"
 #include <stdio.h>
 #include <time.h>
+#include <sys/time.h>
 
 static event_handler_t event_handlers[STATE_N][EVENT_N] = {
         [LEADER][LOG_UPDATE] = log_update_handler
 };
+delete_callback_t delete_callback;
+edit_callback_t edit_callback;
 
 
-void init() {
+void run(delete_callback_t delete, edit_callback_t edit) {
+    delete_callback = delete;
+    edit_callback = edit;
     time_t t;
     srand((unsigned)time(&t));
     node_init(& self);
